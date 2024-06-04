a1f1cd95f6d5b29a711e57075403229cc0c3ba8d
NTLong-Dev <tuuyen19112911@gmail.com>
2024-06-04 22:54:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var a = document.getElementById('a');
+var b = document.getElementById('b');
+var c = document.getElementById('kq');
+var thuong = document.getElementById("thuong");
+
+
+thuong.addEventListener('click' , function(){
+    alert('bạn');
+});
+
